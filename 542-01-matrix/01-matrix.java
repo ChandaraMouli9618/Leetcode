39753b0636fdf1f1b4d056af5aa048105971d5a2
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2024-06-05 00:47:19 +0530

    Time: 425 ms (5.01%) | Memory: 47.6 MB (32.54%) - LeetSync

---
+++ 542-01-matrix/01-matrix.java
@@ -0,0 +1,53 @@
+class Solution {
+
+    int[][] distances;
+    int[] X = {0, 1, 0, -1};
+    int[] Y = {1, 0, -1, 0};
+
+    public int[][] updateMatrix(int[][] mat) {
+        int m = mat.length;
+        int n = mat[0].length;
+        distances = new int[m][n];
+        for(int[] arr : distances) Arrays.fill(arr, Integer.MAX_VALUE);
+
+        Queue<Integer[]> queue = new LinkedList<>();;
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(mat[i][j] == 0){
+                    queue.add(new Integer[]{i, j});
+                    distances[i][j] = 0;
+                }
+            }
+        }
+
+        int steps = 1;
+        while(!queue.isEmpty()){
+            int size = queue.size();
+            System.out.println(size);
+            for(int i = 0; i < size; i++){
+                Integer[] coords = queue.poll();
+                System.out.println(Arrays.toString(coords));
+                for(int j = 0; j < 4; j++){
+                    
+                    int x = X[j] + coords[0];
+                    int y = Y[j] + coords[1];
+                    
+                    if( isValid(x, y, m, n, steps, distances) ){
+                        distances[x][y] = steps;
+                        queue.add(new Integer[]{x, y});
+                    }
+                }
+            }
+            
+            steps++;
+        }
+
+        return distances;
+    }
+
+    boolean isValid(int x, int y, int m, int n, int steps, int[][] distances){
+        if(x < 0 || y < 0 || x >= m || y >= n || steps >= distances[x][y]) return false;
+        return true;
+    }
+    
+}
