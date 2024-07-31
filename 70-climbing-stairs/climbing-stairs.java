8a494ff7ec157f83352b01726660b8696ee2df94
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2024-07-31 20:24:47 +0530

    Time: 0 ms (100.00%) | Memory: 40.1 MB (57.89%) - LeetSync

--- 70-climbing-stairs/climbing-stairs.java
+++ 70-climbing-stairs/climbing-stairs.java
@@ -1,15 +1,16 @@
 class Solution {
+    int[] dp;
     public int climbStairs(int n) {
-        if(n == 1) return 1;
-        int a = 1;
-        int b = 2;
+        dp = new int[n+1];
+        Arrays.fill(dp, -1);
+        return stairClimbWayCount(n);
+    }
 
-        for(int i = 3; i <= n; i++){
-            int c = a + b;
-            a = b;
-            b = c;
-        }
-
-        return b;
+    int stairClimbWayCount(int n){
+        if(n == 0) return 1;
+        if(n < 0) return 0;
+        if(dp[n] != -1) return dp[n];
+        //System.out.println(n);
+        return dp[n] = stairClimbWayCount(n-1) + stairClimbWayCount(n-2);
     }
 }
