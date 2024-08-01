bd6fa8e84bf05ba4d6933e40e46a6216f6a8203d
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2024-08-01 18:47:10 +0530

    Time: 3 ms (62.81%) | Memory: 45.7 MB (14.67%) - LeetSync

---
+++ 64-minimum-path-sum/minimum-path-sum.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int minPathSum(int[][] grid) {
+        int m = grid.length;
+        int n = grid[0].length;
+        for(int i = 1; i < n; i++) grid[0][i] += grid[0][i-1];
+         for(int i = 1; i < m; i++) grid[i][0] += grid[i-1][0];
+
+        for(int row = 1; row < m; row++){
+            for(int col = 1; col < n; col++){
+                grid[row][col] += Math.min(grid[row][col-1], grid[row-1][col]);
+            }
+        }
+
+        return grid[m-1][n-1];
+    }
+}
