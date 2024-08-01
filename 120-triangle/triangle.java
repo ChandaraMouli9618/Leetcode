4ae7350a275abc4ffed64a8d0554d900a4ffc465
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2024-08-01 18:35:54 +0530

    Time: 3 ms (64.63%) | Memory: 44 MB (51.19%) - LeetSync

--- 120-triangle/triangle.java
+++ 120-triangle/triangle.java
@@ -1,24 +1,20 @@
 class Solution {
-public int minimumTotal(List<List<Integer>> triangle){
-	int n = triangle.size();
-	int[][] dp = new int[n][n];
-	for(int[] arr : dp) Arrays.fill(arr, -1);
+    public int minimumTotal(List<List<Integer>> triangle){
+	    int n = triangle.size();
+	    int[][] dp = new int[n][n];
+	    for(int[] arr : dp) Arrays.fill(arr, -1);
+        for(int i = 0; i < n; i++){
+            dp[n-1][i] = triangle.get(n-1).get(i);
+        }
 
-	return minPathSum(0, 0, triangle, n, dp);
-}
-
-int minPathSum(int level, int pos, 
-List<List<Integer>> triangle, int n, int[][] dp){
-
-	if(level == n) return 0;
-	if(dp[level][pos] != -1) return dp[level][pos];
-
-	int leftPathSum = minPathSum(level+1, pos, triangle, n, dp);
-	int rightPathSum = minPathSum(level+1, pos+1, triangle, n, dp);
-
-	return dp[level][pos] = Math.min(leftPathSum, rightPathSum) + 
-triangle.get(level).get(pos);
-}
-
-
+	    for(int level = n-2; level >= 0; level--){
+            for(int pos = level; pos >= 0; pos--){
+	            int leftPathSum = dp[level+1][pos];
+	            int rightPathSum = dp[level+1][pos+1];
+	            dp[level][pos] = Math.min(leftPathSum, rightPathSum) + triangle.get(level).get(pos);
+            }
+        }
+        
+        return dp[0][0];
+    }
 }
