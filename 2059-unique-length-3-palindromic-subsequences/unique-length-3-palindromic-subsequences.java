e9842ac69d33615f80b93e2389c143f0034a0a0f
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-01-04 11:15:43 +0530

    Time: 45 ms (86.65%) | Memory: 44.6 MB (99.74%) - LeetSync

---
+++ 2059-unique-length-3-palindromic-subsequences/unique-length-3-palindromic-subsequences.java
@@ -0,0 +1,38 @@
+class Solution {
+    public int countPalindromicSubsequence(String s) {
+        int[][] range = new int[26][2];
+        boolean[][] dp = new boolean[26][26];
+        for(int[] arr : range){
+            arr[0] = Integer.MAX_VALUE;
+            arr[1] = Integer.MIN_VALUE;
+        }
+
+        for(boolean[] arr : dp){
+            Arrays.fill(arr, false);
+        }
+        for(int i = 0; i < s.length(); i++){
+            int charIndex = s.charAt(i)-'a';
+            if(range[charIndex][0] >= i) range[charIndex][0] = i;
+            if(range[charIndex][1] < i) range[charIndex][1] = i;
+        }
+
+        for(int i = 1; i < s.length()-1; i++){
+            int charIndex = s.charAt(i) - 'a';
+            for(int j = 0; j < 26; j++){
+                if(range[j][0] < i && range[j][1] > i){
+                    dp[j][charIndex] = true;
+                }
+            }
+        }
+
+        int res = 0;
+
+        for(int i = 0; i < dp.length; i++){
+            for(int j = 0; j < dp[0].length; j++){
+                if(dp[i][j]) res++;
+            }
+        }
+
+        return res;
+    }
+}
