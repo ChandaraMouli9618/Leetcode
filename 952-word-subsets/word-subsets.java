e84cf34903aa8755076c798ef12508d3386f1789
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-01-10 16:37:14 +0530

    Time: 15 ms (30.23%) | Memory: 54.8 MB (64.53%) - LeetSync

---
+++ 952-word-subsets/word-subsets.java
@@ -0,0 +1,38 @@
+class Solution {
+    public List<String> wordSubsets(String[] words1, String[] words2) {
+        List<String> res = new ArrayList<>();
+        int[] maxFreqs = new int[26];
+
+        for (String word : words2) {
+            int[] currFreq = countCharFreq(word);
+            for (int i = 0; i < 26; i++)
+                maxFreqs[i] = Math.max(maxFreqs[i], currFreq[i]);
+        }
+
+        for (String word : words1) {
+            int[] baseWordFreq = countCharFreq(word);
+            int i;
+
+            for (i = 0; i < 26; i++) {
+                if (baseWordFreq[i] < maxFreqs[i])
+                    break;
+            }
+            if (i == 26)
+                res.add(word);
+        }
+
+        return res;
+
+    }
+
+    int[] countCharFreq(String word) {
+        int[] freq = new int[26];
+        Arrays.fill(freq, 0);
+
+        for (char ch : word.toCharArray()) {
+            freq[ch - 'a']++;
+        }
+
+        return freq;
+    }
+}
