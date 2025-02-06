50bbe02e35b24bb889b7f314f37b1aab24bbdd89
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-02-06 17:37:28 +0530

    Time: 196 ms (73.72%) | Memory: 68.3 MB (76.92%) - LeetSync

--- 1364-tuple-with-same-product/tuple-with-same-product.java
+++ 1364-tuple-with-same-product/tuple-with-same-product.java
@@ -2,20 +2,14 @@ class Solution {
     public int tupleSameProduct(int[] nums) {
         int n = nums.length;
         Map<Integer, Integer> map = new HashMap<>();
+        int res = 0;
         for(int i = 0 ; i < n-1 ; i++){
             for(int j = i + 1; j < n; j++){
                 int product = nums[i] * nums[j];
-                map.put(product, map.getOrDefault(product, 0)+1);
+                int currFreq = map.getOrDefault(product, 0);
+                res += (currFreq) * 8;
+                map.put(product, currFreq+1);
             }
-        }
-        int res = 0;
-        for(Integer key : map.keySet()){
-            // System.out.println(key + " -> " + map.get(key));
-            int freq = map.get(key);
-                if(freq > 1){
-                    res += ((freq * (freq-1))/2) * 8;
-                }
-
         }
 
         return res;
