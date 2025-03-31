e335399a31ba98d7de7cf424b0bc570629a18e66
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-03-31 18:56:48 +0530

    Time: 0 ms (100.00%) | Memory: 41.4 MB (15.23%) - LeetSync

--- 198-house-robber/house-robber.java
+++ 198-house-robber/house-robber.java
@@ -1,20 +1,16 @@
 class Solution {
     public int rob(int[] nums) {
-        int n= nums.length;
-        int res = 0;
+        int n = nums.length;
+        if(n == 1) return nums[0];
 
-        int a = nums[0];
-        if(n == 1) return a;
+        int[] dp = new int[n];
+        dp[0] = nums[0];
+        dp[1] = Math.max(nums[0], nums[1]);
 
-        int b = Math.max(nums[0], nums[1]);
-        if(n == 2) return b;
-
-        for(int i = 2; i < n; i++){
-            res = Math.max(b, a + nums[i]);
-            a = b;
-            b = res;
+        for(int i = 2; i < n ;i++){
+            dp[i] = Math.max(dp[i-2] + nums[i], dp[i-1]);
         }
 
-        return res;
+        return dp[n-1];
     }
 }
