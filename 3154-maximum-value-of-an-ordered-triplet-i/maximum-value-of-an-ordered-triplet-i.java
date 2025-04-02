68acbdd6f74887eb62bb7810ff4c6435a7874eda
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-04-02 15:47:56 +0530

    Time: 1 ms (99.31%) | Memory: 41.9 MB (86.60%) - LeetSync

--- 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.java
+++ 3154-maximum-value-of-an-ordered-triplet-i/maximum-value-of-an-ordered-triplet-i.java
@@ -1,14 +1,19 @@
 class Solution {
     public long maximumTripletValue(int[] nums) {
-        long res = 0;
         int n = nums.length;
+        int[] maxRight = new int[n];
+        maxRight[n-1] = -1;
 
-        for(int i = 0; i < n - 2; i++){
-            for(int j = i+1; j < n-1; j++){
-                for(int k = j + 1; k < n; k++){
-                    res = Math.max(res, (long)(nums[i] - nums[j]) * nums[k]);
-                }
-            }
+        for(int i = n-2; i >= 0; i--){
+            maxRight[i] = Math.max(nums[i+1], maxRight[i+1]);
+        }
+
+        int leftMax = nums[0];
+        long res = 0;
+        for(int i = 1; i < n-1; i++){
+            //System.out.println("Left Max : " + leftMax + ", Right max : " + maxRight[i] + ", " + "Num : " + nums[i]);
+            res = Math.max(res, ((long)(leftMax-nums[i]) * maxRight[i]));
+            leftMax = Math.max(leftMax, nums[i]);
         }
 
         return res;
