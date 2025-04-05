7727398a8e848ae1bb558907dd7b5d0ecbe9486a
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-04-05 20:19:25 +0530

    Time: 0 ms (100.00%) | Memory: 40.7 MB (36.20%) - LeetSync

--- 62-unique-paths/unique-paths.java
+++ 62-unique-paths/unique-paths.java
@@ -1,15 +1,13 @@
 class Solution {
     public int uniquePaths(int m, int n) {
         int[][] dp = new int[m][n];
-        for(int[] arr : dp) Arrays.fill(arr, -1);
-        return pathCountUtil(0, 0, m, n, dp);
-    }
+        for(int i = 0; i < m; i++){
+            for(int j = 0; j < n; j++){
+                if(i == 0 || j == 0) dp[i][j] = 1;
+                else dp[i][j] = dp[i][j-1] + dp[i-1][j];
+            }
+        }
 
-    int pathCountUtil(int x, int y, int m, int n, int[][] dp){
-        if(x == m || y == n) return 0;
-        if(dp[x][y] != -1) return dp[x][y];
-        if(x == m-1 && y == n-1) return 1;
-
-        return dp[x][y] = pathCountUtil(x+1, y, m, n, dp) + pathCountUtil(x, y+1, m, n, dp);
+        return dp[m-1][n-1];
     }
 }
