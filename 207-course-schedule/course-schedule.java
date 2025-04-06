aa581a16f04897f07c0a29b0b5c301400d7ffef2
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-04-06 14:51:40 +0530

    Time: 4 ms (91.06%) | Memory: 45 MB (89.42%) - LeetSync

--- 207-course-schedule/course-schedule.java
+++ 207-course-schedule/course-schedule.java
@@ -1,33 +1,31 @@
 class Solution {
-    char[] courseStatus;
-    List<List<Integer>> adj;
-    public boolean canFinish(int numCourses, int[][] prerequisites) {
-        courseStatus = new char[numCourses];
+    public boolean canFinish(int numCourses, int[][] prereqs) {
+        List<List<Integer>> adj = new ArrayList<>();
+        for(int i = 0; i < numCourses; i++) adj.add(new ArrayList<>());
+        for(int[] req : prereqs) adj.get(req[1]).add(req[0]);
+        char[] courseStatus = new char[numCourses];
         Arrays.fill(courseStatus, 'N');
-        adj = new ArrayList<>();
-        for(int i = 0; i < numCourses; i++) adj.add(new ArrayList<Integer>());
-        for(int[] prerequisite : prerequisites) adj.get(prerequisite[0]).add(prerequisite[1]);
 
         for(int i = 0; i < numCourses; i++){
             if(courseStatus[i] == 'N'){
-                if(!dfs(i)) return false;
+                boolean isPossible = courseCheckUtil(i, adj, courseStatus);
+                if(!isPossible) return false;
             }
         }
 
         return true;
     }
 
-    boolean dfs(int pos){
-        if(courseStatus[pos] == 'C') return true;
-        if(courseStatus[pos] == 'P') return false;
+    boolean courseCheckUtil(int node, List<List<Integer>> adj, char[] courseStatus){
+        if(courseStatus[node] == 'Y') return true;
+        if(courseStatus[node] == 'P') return false;
 
-        courseStatus[pos] = 'P';
-
-        for(int i : adj.get(pos)){
-            if(!dfs(i)) return false;
+        courseStatus[node] = 'P';
+        for(int reqCourse : adj.get(node)){
+            boolean courseCheck = courseCheckUtil(reqCourse, adj, courseStatus);
+            if(!courseCheck) return false;
         }
-
-        courseStatus[pos] = 'C';
+        courseStatus[node] = 'Y';
         return true;
     }
 }
