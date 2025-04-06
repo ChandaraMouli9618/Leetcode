c9960338105cf09b5d1fa275d864838715981688
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-04-06 17:43:54 +0530

    Time: 1 ms (99.42%) | Memory: 45.8 MB (91.02%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.java
@@ -0,0 +1,17 @@
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = 0;
+        int right = 0;
+        int n = nums.length;
+
+        while(right < n){
+            if(nums[right] != 0){
+                nums[left] = nums[right];
+                left++;
+            }
+            right++;
+        }
+
+        while(left < n) nums[left++] = 0;
+    }
+}
