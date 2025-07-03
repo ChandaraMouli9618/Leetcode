31ea44e488b0d5508f75dcee61fe01bcdc57486a
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-07-03 21:13:47 +0530

    Time: 0 ms (100.00%) | Memory: 41.5 MB (87.67%) - LeetSync

--- 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
+++ 3600-find-the-k-th-character-in-string-game-i/find-the-k-th-character-in-string-game-i.java
@@ -1,20 +1,17 @@
 class Solution {
     public char kthCharacter(int k) {
-        String src = "a";
-        while(src.length() < k){
-            src = getNextString(src);
-            //System.out.println(src);
-        }
-
-        return src.charAt(k-1);
+        return helper(k-1);
     }
 
-    private String getNextString(String str){
-        StringBuilder nextString = new StringBuilder("");
-        for(char ch : str.toCharArray()){
-            nextString.append((char)(((ch - 'a' + 1) % 26) + 'a'));
-        }
 
-        return str + nextString.toString();
+    char helper(int i){
+        if(i == 0) return 'a';
+        char res = helper(i/2);
+
+        if(i%2 == 1) {
+            if(res == 'z') return 'a';
+            return (char)(res + 1);
+        }
+        return res;
     }
 }
