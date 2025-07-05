e8e9304e2344d75640f96bfb267a04b2768d64f3
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2025-07-05 13:00:39 +0530

    Time: 1 ms (99.94%) | Memory: 43.5 MB (43.80%) - LeetSync

--- 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
+++ 1510-find-lucky-integer-in-an-array/find-lucky-integer-in-an-array.java
@@ -1,15 +1,8 @@
 class Solution {
     public int findLucky(int[] arr) {
-        Map<Integer, Integer> freq = new HashMap<>();
-        for(int i : arr) freq.put(i, freq.getOrDefault(i, 0)+1);
-        int res = -1;
-
-        for(Map.Entry<Integer, Integer> entry : freq.entrySet()){
-            int key = entry.getKey();
-            int value = entry.getValue();
-            if( key == value && key > res) res = key;
-        }
-
-        return res;
+        int[] freq = new int[501];
+        for(int i : arr) freq[i]++;
+        for(int i = freq.length - 1; i >= 1; i--) if(freq[i] == i) return i;
+        return -1;
     }
 }
