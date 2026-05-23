a909c06136c79a306ccf7c8a22319c548f720f4b
Expect The Unexpected <91553226+ChandaraMouli9618@users.noreply.github.com>
2026-05-23 22:42:22 +0530

    Time: 0 ms (100.00%) | Memory: 43 MB (44.94%) - LeetSync

--- 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
+++ 1878-check-if-array-is-sorted-and-rotated/check-if-array-is-sorted-and-rotated.java
@@ -1,12 +1,15 @@
 class Solution {
     public boolean check(int[] nums) {
-        int n = nums.length;
         int breakPoint = 0;
-        for(int i = 1; i < n; i++){
-            if(nums[i] < nums[i-1]) breakPoint++;
+        int n = nums.length;
+        for(int i = 1;i < n; i++){
+            if(nums[i] < nums[i-1]){
+                breakPoint++;
+                if(breakPoint > 1) return false;
+            }
         }
 
-        if(breakPoint == 0) return true;
-        return nums[0] >= nums[n-1] && breakPoint == 1;
+        if(breakPoint == 1 && nums[0] < nums[n-1]) return false;
+        return true;
     }
 }
